6891b527c1279c275820bf7b20c3cf29585a44dd
tav <tav@espians.com>
2009-02-16 09:45:19 +0000

    Updated the script to reflect the new app/ structure.

--- bin/update.sh
+++ bin/update.sh
@@ -2,5 +2,5 @@
 
 tweetapp_root=$(dirname $(dirname $0))
 
-echo "APPLICATION_TIMESTAMP = `python -c "from time import time; print time()"`" > $tweetapp_root/source/updated.py
-$tweetapp_root/google_appengine/appcfg.py -v update $tweetapp_root/source
+echo "APPLICATION_TIMESTAMP = `python -c "from time import time; print time()"`" > $tweetapp_root/app/source/updated.py
+$tweetapp_root/google_appengine/appcfg.py -v update $tweetapp_root/app
