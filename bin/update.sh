7530186499c60ffe28bdea7a7e6bdd8fc48da005
tav <tav@espians.com>
2009-05-22 02:44:56 +0100

    Cleaned up Tweetapp and now with POST support thanks to @nlehuen.

--- bin/update.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-tweetapp_root=$(dirname $(dirname $0))
-
-echo "APPLICATION_TIMESTAMP = `python -c "from time import time; print time()"`" > $tweetapp_root/app/source/updated.py
-$tweetapp_root/google_appengine/appcfg.py -v update $tweetapp_root/app
